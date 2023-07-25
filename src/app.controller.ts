febea9c5f43248bb969a0deeeefbf13debc5e54c
NehaShinde01 <116095704+NehaShinde01@users.noreply.github.com>
2023-07-25 08:54:22 +0000

    added user code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,35 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get('/users')
+  getUsers(): User[] {
+    return this.appService.getUsers();
+  }
+
+  @Post('/users')
+  createUser(@Body() userDetails: Partial<User>): User {
+    return this.appService.createUser(userDetails);
+  }
+
+  @Get('/users/:id')
+  getUser(@Param('id') id: number): User {
+    return this.appService.getUser(id);
+  }
+
+  @Patch('/users/:id')
+  updateUser(@Param('id') id: number, @Body() userDetails: Partial<User>): User {
+    return this.appService.updateUser(id, userDetails);
+  }
+
+  @Delete('/users/:id')
+  deleteUser(@Param('id') id: number): boolean {
+    return this.appService.deleteUser(id);
+  }
+
+  @Get('/users/search/:query')
+  searchUser(@Param('query') query: string): User[] {
+    return this.appService.searchUser(query);
   }
 }
