7b23f195e6a4ab51856c93e1463c3445d524ff51
NehaShinde01 <116095704+NehaShinde01@users.noreply.github.com>
2023-07-29 11:53:04 +0000

    added Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();//it gives us an permission to acces API from any server
   await app.listen(3000);
 }
 bootstrap();
